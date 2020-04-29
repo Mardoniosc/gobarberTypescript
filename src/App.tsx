bbe46c6042dc23530b2b4533e232f51d1f7599eb
Mardonio Costa <mardonio@live.com>
2020-04-29 19:58:59 -0300

    Estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
-const App: React.FC = () => <h1>hello word</h1>
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <GlobalStyle />
+    <h1>hello word</h1>;
+  </>
+);
 
 export default App;
