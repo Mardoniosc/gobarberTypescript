aad5c1289a307b8df3b97221832b0cc3c79b2142
Mardonio Costa <mardonio@live.com>
2020-05-04 18:38:27 -0300

    Api de Contexto

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useCallback } from 'react';
+import React, { useRef, useCallback, useContext } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
@@ -8,12 +8,15 @@ import logoImg from '../../assets/logo.svg';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
+import AuthContext from '../../context/AuthContext';
 
 import { Container, Content, Background } from './styles';
 import getValidationErros from '../../utils/getValidationErrors';
 
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
+
+  const auth = useContext(AuthContext);
 
   const handleSubmit = useCallback(async (data: object) => {
     try {
