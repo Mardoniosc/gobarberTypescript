f7e098050a61553d1580d49407bd89c49de23966
Mardonio Costa <mardonio@live.com>
2020-05-10 13:10:32 -0300

    Criado um botão de lotou

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { useAuth } from '../../hooks/Auth';
+import Button from '../../components/Button';
 
-import { Container } from './styles';
+import { Container, Content } from './styles';
 
-const Dashboard: React.FC = () => <h1>Dashboard</h1>;
+const Dashboard: React.FC = () => {
+  const { signOut } = useAuth();
+  const handleLogout = useCallback(() => {
+    signOut();
+  }, []);
+
+  return (
+    <Container>
+      <Content>
+        <h1>Dashboard</h1>
+        <Button type="button" onClick={handleLogout}>
+          Logout
+        </Button>
+      </Content>
+    </Container>
+  );
+};
 
 export default Dashboard;
